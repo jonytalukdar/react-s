c4592a3679ad165ac961f41ae7f5cbf09919ca8e
jonytalukdar <jonytalukdar30@gmail.com>
2021-08-19 17:44:48 +0600

    added tasktraker

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import Clock from './components/Events/Clock';
 
 class App extends Component {
   render() {
     console.log('app comonent render');
     return (
       <div>
-        <Clock />
+        <h1>hello</h1>
       </div>
     );
   }
